7601eef6bc75d48f51aba6f6b334861f99d5dffa
Austin Jess <ajess33@gmail.com>
2018-08-07 20:43:05 -0500

    Refactored Routes

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -1,31 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models');
+const helpers = require('../helpers/todos');
 
-router.get('/', (req, res) => {
-  db.Todo.find()
-    .then((todos) => res.json(todos))
-    .catch((err) => res.send(err));
-});
+router
+  .route('/')
+  .get(helpers.getTodos)
+  .post(helpers.createTodo);
 
-router.post('/', (req, res) => {
-  db.Todo.create(req.body)
-    .then((newTodo) => res.status(201).json(newTodo))
-    .catch((err) => res.send(err));
-});
-
-router.get('/:todoId', (req, res) => {
-  // req.params is how you access the value of '/:todoId'
-  db.Todo.findById(req.params.todoId)
-    .then((foundTodo) => res.json(foundTodo))
-    .catch((err) => res.send(err));
-});
-
-router.put('/:todoId', (req, res) => {
-  // req.body will contain our info to update
-  db.Todo.findOneAndUpdate({ _id: req.params.todoId }, req.body, { new: true })
-    .then((todo) => res.json(todo))
-    .catch((err) => res.send(err));
-});
+router
+  .route('/:todoId')
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo);
 
 module.exports = router;
