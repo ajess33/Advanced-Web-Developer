e580c94bb01b183c53f0d605fc97a5a86832b06d
Austin Jess <ajess33@gmail.com>
2018-08-10 17:17:30 -0500

    Added Toggle Logic

--- fullstack_todo_app/todosfrontend/src/TodoList.js
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -91,6 +91,41 @@ class TodoList extends Component {
       });
   };
 
+  toggleTodo = (todo) => {
+    // need '_id' because we are passing in whole todo instead of just 'id'
+    const updateURL = APIURL + todo._id;
+    fetch(updateURL, {
+      method: 'put',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({ completed: !todo.completed })
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          if (resp.status >= 400 && resp.status < 500) {
+            return resp.json().then((data) => {
+              let err = { errorMessage: data.message };
+              throw err;
+            });
+          } else {
+            let err = {
+              errorMessage: 'Please try again. Server did not respond'
+            };
+            throw err;
+          }
+        }
+        return resp.json();
+      })
+      .then((updatedTodo) => {
+        const todos = this.state.todos.map(
+          (t) =>
+            t._id === updatedTodo._id ? { ...t, completed: !t.completed } : t
+        );
+        this.setState({ todos: todos });
+      });
+  };
+
   render() {
     const todos = this.state.todos.map((t) => (
       // bind gives us the id argument to work with
@@ -98,6 +133,7 @@ class TodoList extends Component {
         key={t._id}
         {...t}
         onDelete={this.deleteTodo.bind(this, t._id)}
+        onToggle={this.toggleTodo.bind(this, t)}
       />
     ));
     return (
