733cf9dfeae7a6ca142b64650ba5f23f21786281
Austin Jess <ajess33@gmail.com>
2018-08-10 16:56:11 -0500

    Added Delete Logic

---
+++ fullstack_todo_app/todosfrontend/src/TodoForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+  state = {
+    inputValue: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      inputValue: e.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    this.props.addTodo(this.state.inputValue);
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}>Add Todo</button>
+      </div>
+    );
+  }
+}
+
+export default TodoForm;
--- fullstack_todo_app/todosfrontend/src/TodoList.js
+++ fullstack_todo_app/todosfrontend/src/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
-const APIURL = '/api/todos';
+import TodoForm from './TodoForm';
+const APIURL = '/api/todos/';
 
 class TodoList extends Component {
   state = {
@@ -34,11 +35,75 @@ class TodoList extends Component {
       .then((todos) => this.setState({ todos }));
   };
 
+  addTodo = (val) => {
+    fetch(APIURL, {
+      method: 'post',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({ name: val })
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          if (resp.status >= 400 && resp.status < 500) {
+            return resp.json().then((data) => {
+              let err = { errorMessage: data.message };
+              throw err;
+            });
+          } else {
+            let err = {
+              errorMessage: 'Please try again. Server did not respond'
+            };
+            throw err;
+          }
+        }
+        return resp.json();
+      })
+      .then((newTodo) => {
+        this.setState({ todos: [this.state.todos, newTodo] });
+      });
+  };
+
+  deleteTodo = (id) => {
+    const deleteURL = APIURL + id;
+    fetch(deleteURL, {
+      method: 'delete'
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          if (resp.status >= 400 && resp.status < 500) {
+            return resp.json().then((data) => {
+              let err = { errorMessage: data.message };
+              throw err;
+            });
+          } else {
+            let err = {
+              errorMessage: 'Please try again. Server did not respond'
+            };
+            throw err;
+          }
+        }
+        return resp.json();
+      })
+      .then(() => {
+        const todos = this.state.todos.filter((todo) => todo._id !== id);
+        this.setState({ todos: todos });
+      });
+  };
+
   render() {
-    const todos = this.state.todos.map((t) => <TodoItem key={t._id} {...t} />);
+    const todos = this.state.todos.map((t) => (
+      // bind gives us the id argument to work with
+      <TodoItem
+        key={t._id}
+        {...t}
+        onDelete={this.deleteTodo.bind(this, t._id)}
+      />
+    ));
     return (
       <div>
         <h1>Todo List</h1>
+        <TodoForm addTodo={this.addTodo} />
         <ul>{todos}</ul>
       </div>
     );
